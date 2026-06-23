bd524c925ca6e1bdbd1e0d760b3e6c362783c5c2
Jen Wall <74634137+wall-jennifer@users.noreply.github.com>
2026-06-23 12:04:09 -0400

    Update 1.2_COPI_climate-filtering-occupancy.R

--- code/1.2_COPI_climate-filtering-occupancy.R
+++ code/1.2_COPI_climate-filtering-occupancy.R
@@ -3,14 +3,10 @@
 
 ### Hierarchical single-species, single-season occupancy model of local, microclimate factors
 
-
 # Metadata:
-# Visibility in km, >2 km visibility listed as 2
 # Cloud: in % cloud cover of visible sky
-# Wind: scaled from ________
-# Talus.Score: talus scored on stability from 1 - 5
 # Avg. Diameter: in cm, avg. scores, for small and large rocks, we chose predominant rock size (whichever was >50%)
-# Vegetation: scored based on Daubenmire's scale-- 0 = 0, trace = 1, 1-5 = 2, 5-25 = 3, 25-50 = 4, 50-75 = 5, 75-95 = 6, 95-99 = 7, 100 = 8
+# Vegetation: scored based on Daubenmire's scale-- 0 = 0, trace = 1, 1-5 = 2, 5-25 = 3, 25-50 = 4, 50-75 = 5, 75-95 = 6, 95-99 = 7, 100 = 8 (gram and forb summed for gramforb measure)
 # Species: X for not present, P for present, O for old, F for fresh
 
 
@@ -25,21 +21,8 @@ library(jagsUI)     # interfaces with JAGS
 library(mcmcOutput) # interprets results
 
 # Import and organize data
-COPI <- read.csv("3.Analysis/Talussurveys_20212022_COPI_2025FinalVersion.csv") %>% 
-  select(-X, -ID) %>% pivot_wider(names_from = "Type", values_from = "Obs") %>% 
-  mutate(Scat = as.integer(ifelse(Scat >= 1, 1, 0)), row_num = row_number())
-
-COPIdet_data <- as.data.frame(scale(COPI[,c("cloud", "avgdiam", "Wind", "Talus.Score", "gram", 
-                                            "forb", "gramforb", "snow", "slope", "perm", "ros", 
-                                            "sum.days.over.21", "wint.days.below.neg5", "max.sum",
-                                            "min.wint", "wint.days.above.0", "evi")])) %>% 
-  mutate(row_num = row_number()) %>% 
-  left_join(COPI[,c("row_num", "Sp", "y", "x", "doy", "Year", "Month", "Day", "Start", 
-                    "Indiv", "Scat", 'Vocal', "Haypile")], ., by = "row_num") %>% 
-  filter(!is.na(avgdiam) & !is.na(gramforb))
-# filtered out the rows with NA values (3 in total) left with 83 sites
-
-data.COPI <- read.csv("3.Analysis/Talussurveys_20212022_COPI_2025_withCoOccurrences.csv")
+data.COPI <- read.csv("data/Ramdomized-talus-surveys-COPI.csv") %>% 
+  mutate(perm = ifelse(perm == 104, 0, perm))  # 104 classification stands for barren, a non-permafrost region
 scaled.COPI <- as.data.frame(scale(data.COPI[,c("gramforb", "avgdiam", "cloud")]))
 
 # Fit the model
@@ -47,10 +30,6 @@ J <- 83
 K <- 3
 
 # Organize the data
-jags_data = list(J = J, K = K, det_data = COPIdet_data[,c("Indiv", "Scat", 'Vocal')], 
-                 avgdiam = COPIdet_data$avgdiam, gramforb = COPIdet_data$gramforb,
-                 cloud = COPIdet_data$cloud)
-
 COPI.jags_data = list(J = J, K = K, y = data.COPI[, c("Scat", "Indiv", "Vocal")], 
                       Z = ifelse(rowSums(data.COPI[, c("Scat", "Indiv", "Vocal")], na.rm = T) > 0, 1, NA),
                       cloud = scaled.COPI$cloud, avgdiam = scaled.COPI$avgdiam, gramforb = scaled.COPI$gramforb)
